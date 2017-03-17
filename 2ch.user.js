f0ee1943061ad0fcb551c61485d9290ea1b71405
asm-jaime <asm.jaime@gmail.com>
2017-03-18 00:39:09 +1000

    add preview

--- 2ch.user.js
+++ 2ch.user.js
@@ -1,4 +1,5 @@
 // ==UserScript==
+// @autor       Anonymous
 // @name        LaTeXhk
 // @namespace   LaTeXhk
 // @include     /^https?:\/\/2ch\.(hk|pm|re|tf|wj)/(sci|test)/
@@ -11,13 +12,13 @@
  
 mathJaxOpt = document.createElement('script');
 mathJaxOpt.type = 'text/x-mathjax-config';
-mathJaxOpt[(mathJaxOpt.innerText===undefined?"textContent":"innerText")] = 'MathJax.Hub.Config({tex2jax: {inlineMath: [[\'$\',\'$\']]}});';
+mathJaxOpt[(mathJaxOpt.innerText==undefined?"textContent":"innerText")] = 'MathJax.Hub.Config({AsciiMath: { fixphi: true, useMathMLspacing: true, displaystyle: false, decimalsign: "."}, tex2jax: {inlineMath: [[\'$\',\'$\']]}});'; 
 document.getElementsByTagName('head')[0].appendChild(mathJaxOpt);
 mathJax = document.createElement('script');
 mathJax.type = 'text/javascript';
-mathJax.src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+mathJax.src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_HTMLorMML"; // only asciiMath AM_HTMLorMML, Only LaTeX and TeX TeX-AMS-MML_HTMLorMML, for all TeX-MML-AM_HTMLorMML
 document.getElementsByTagName('head')[0].appendChild(mathJax);
- 
+//area footer 
 previewArea = document.createElement('tr');
 previewArea.className = "preview-area";
 previewAreaLabelCont = document.createElement('td');
@@ -31,24 +32,49 @@ preview = document.createElement('td');
 preview.className = "preview";
 previewArea.appendChild(preview);
 var options = document.getElementsByClassName('option-area desktop')[0];
- 
+
+//area short window
+previewWindow = document.createElement('div');
+previewWindow.className = "preview-window";
+var optionsWindow = document.getElementsByClassName('qr-mail')[0];
+
+function ClickPreviewButton(){
+  if (options.parentNode!==previewArea.parentNode){
+    options.parentNode.insertBefore(previewArea,options);
+  }
+  optionsWindow.parentNode.insertBefore(previewWindow,optionsWindow);
+  var html =document.getElementsByClassName('symbol-counter')[0].children[1].value;
+  html = html.replace(/\n/g, "<br>");
+  preview.innerHTML=html;
+  previewWindow.innerHTML=html;  
+  window.postMessage("latex_update", "*");
+  };
+  
 previewButton = document.createElement('input');
 previewButton.id = 'previewButton';
 previewButton.className = 'all-platform';
 previewButton.type = 'button';
 previewButton.value = 'Предпросмотр';
 previewButton.name = 'previewButton';
-previewButton.onclick = function(){
-  if (options.parentNode!==previewArea.parentNode){
-    options.parentNode.insertBefore(previewArea,options);
-  }
-  var html =document.getElementsByClassName('symbol-counter')[0].children[1].value;
-  html = html.replace(/\n/g, "<br>");
-  preview.innerHTML=html;
-  window.postMessage("latex_update", "*");};
+previewButton.onclick = ClickPreviewButton;
+
+previewButtonQr = document.createElement('input');
+previewButtonQr.id = 'previewButtonQr';
+previewButtonQr.className = 'all-platform';
+previewButtonQr.type = 'button';
+previewButtonQr.value = 'Предпросмотр';
+previewButtonQr.name = 'previewButtonQr';
+previewButtonQr.onclick = ClickPreviewButton;
+
+
+  
 document.getElementById("e-mail").style.width = "250px";
 document.getElementsByClassName('mail')[0].children[1].appendChild(previewButton);
- 
+
+document.getElementById("qr-e-mail").style.width = "300px";
+document.getElementsByClassName('qr-mail')[0].appendChild(previewButtonQr);
+
+
 var script = document.createElement('script');
 script.textContent = '\
 var storedUpdatePosts = updatePosts;\
@@ -64,3 +90,32 @@ window.addEventListener("message", function(e) {\
 ';
 script.type = "text/javascript";
 document.body.appendChild(script);
+
+// ==UserScript==
+// @autor       Anonymous
+// ==/UserScript==
+ 
+ 
+mathJaxOpt = document.createElement('script');
+mathJaxOpt.type = 'text/x-mathjax-config';
+mathJaxOpt[(mathJaxOpt.innerText==undefined?"textContent":"innerText")] = 'MathJax.Hub.Config({AsciiMath: { fixphi: true, useMathMLspacing: true, displaystyle: false, decimalsign: "."}, tex2jax: {inlineMath: [[\'$\',\'$\']]}});'; 
+document.getElementsByTagName('head')[0].appendChild(mathJaxOpt);
+
+mathJax = document.createElement('script');
+mathJax.type = 'text/javascript';
+mathJax.src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_HTMLorMML"; // only asciiMath AM_HTMLorMML, Only LaTeX and TeX TeX-AMS-MML_HTMLorMML, for all TeX-MML-AM_HTMLorMML
+document.getElementsByTagName('head')[0].appendChild(mathJax);
+
+var script = document.createElement('script');
+script.textContent = 'function RedredingMath(){setTimeout("MathJax.Hub.Typeset()",1500);};'; //function(e){if(e.data.indexOf("q_stlast_reloaded")!= -1){console.log(e.data)};} "message", function(e){console.log(e.data);}
+script.type = "text/javascript";
+document.getElementsByTagName('head')[0].appendChild(script);
+
+document.getElementById("im_send").addEventListener("click", RedredingMath);
+
+//script.textContent = ' window.addEventListener("message", function(e){if (e.data == "latex_update") {setTimeout("MathJax.Hub.Typeset();",500);};}, false);';
+//script.textContent = 'setInterval( function() { MathJax.Hub.Typeset(); }, 1000);';
+//function(e){if(e.data.indexOf("q_stlast_reloaded")!= -1){console.log(e.data)};}
+	
+
+
